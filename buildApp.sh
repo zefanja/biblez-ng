339fc68000afa75cadd361e4b6d19d232c309ea0
zefanja <zefanja@freenet.de>
2014-04-16 20:36:39 +0700

    * update buildApp.sh

--- buildApp.sh
+++ buildApp.sh
@@ -8,7 +8,7 @@ echo "\033[1;32mBuilding for Firefox\033[0m"
 cd ../..
 #sh tools/deploy.sh -o github/app
 rm -R github/app
-mkdir -R github/app
+mkdir -p github/app
 cp -a deploy/biblez-ng/. github/app
 cp manifest.desktop github/app/manifest.webapp
 cp biblez.appcache github/app/biblez.appcache
